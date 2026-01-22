fe2088278dc469e59a8897d4c59cc1d0f88215f7
Spectr123 <alex.kim.14032011@gmail.com>
2026-01-22 18:57:14 +0500

    Small improvements in style.

--- src/widget/Header/ui/Header.tsx
+++ src/widget/Header/ui/Header.tsx
@@ -1,14 +1,40 @@
 import styles from "./Header.module.scss";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
     <div className={styles.header}>
-      <Link to="/"><h3>Interview Insights</h3></Link>
+      <NavLink to="/" className={styles.logo}>
+        <h3>Interview Insights</h3>
+      </NavLink>
+
       <div className={styles.line}>
-        <Link to="/interview"><h5>Voice interview</h5></Link>
-        <Link to="/analytics"><h5>Analytics</h5></Link>
-        <Link to="/profile"><h5>Profile</h5></Link>
+        <NavLink
+          to="/interview"
+          className={({ isActive }) =>
+            isActive ? styles.active : styles.link
+          }
+        >
+          <h5>Voice interview</h5>
+        </NavLink>
+
+        <NavLink
+          to="/analytics"
+          className={({ isActive }) =>
+            isActive ? styles.active : styles.link
+          }
+        >
+          <h5>Analytics</h5>
+        </NavLink>
+
+        <NavLink
+          to="/profile"
+          className={({ isActive }) =>
+            isActive ? styles.active : styles.link
+          }
+        >
+          <h5>Profile</h5>
+        </NavLink>
       </div>
     </div>
   );
